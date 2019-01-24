dd76677b00777f4004606da1cd73c9e91fb5d5b6
lakithaomal <lakithaomalanl@gmail.com>
2019-01-24 00:56:02 -0600

    GPS Updates

--- firmware/xu4/mintsXU4/mintsDefinitions.py
+++ firmware/xu4/mintsXU4/mintsDefinitions.py
@@ -14,5 +14,8 @@ dataFolder          = "/media/teamlary/Team_Lary_1/gitGubRepos/data/mintsData/"
 duePort             = findPort("Arduino Due")
 nanoPort            = findPort("FT232R USB UART")
 show2Port           = findPort("CP2104 USB to UART Bridge Controller")
-macAddress          = get_mac_address(interface="docker0").replace(":","")
 # macAddress          = get_mac_address(interface="docker0").replace(":","")
+# macAddress          = get_mac_address(interface="docker0").replace(":","")
+gpsPort           = findPort("GPS/GNSS Receiver")
+
+macAddress          = get_mac_address(interface="enp1s0").replace(":","")
