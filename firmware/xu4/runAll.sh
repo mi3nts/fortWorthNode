e1c202b1a637c67145072e44545910a80a74605d
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2019-01-25 18:31:27 -0600

    Update runAll.sh

--- firmware/xu4/runAll.sh
+++ firmware/xu4/runAll.sh
@@ -3,6 +3,9 @@
 sleep 5
 python3 centralNodeReaderDue.py &
 sleep 5
-python3 centralNodeReaderNano.py &
-sleep 10 
-cd odroidShow2 && python3 mintsShow2.py
+python3 centralNodeReaderGPS.py &
+
+
+# python3 centralNodeReaderNano.py &
+# sleep 10 
+# cd odroidShow2 && python3 mintsShow2.py
