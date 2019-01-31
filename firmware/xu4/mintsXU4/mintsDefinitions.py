1ac7e1ddc246bc99403be4efcfd20bd7b58811a1
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2019-01-30 22:56:47 -0600

    Update mintsDefinitions.py

--- firmware/xu4/mintsXU4/mintsDefinitions.py
+++ firmware/xu4/mintsXU4/mintsDefinitions.py
@@ -10,7 +10,7 @@ def findPort(find):
             return(currentPort.split(" ")[0])
 
 
-dataFolder          = "/home/teamlary/gitHubRepos/data/mintsData/"
+dataFolder          = "/home/teamlary/mintsData/"
 duePort             = findPort("Arduino Due")
 nanoPort            = findPort("FT232R USB UART")
 show2Port           = findPort("CP2104 USB to UART Bridge Controller")
